06e5139bf4e97744838a8edbbdfdf2ebb90b7cef
sherrer08 <74481439+sherrer08@users.noreply.github.com>
2025-04-01 00:33:26 -0400

    Delete components directory

--- components/ResortDetails.js
+++
@@ -1,9 +0,0 @@
-const ResortDetails = ({ resort }) => {
-    return (
-        <div className="home-details">
-            <h4>{resort.name}</h4>
-        </div>
-    )
-}
-
-export default ResortDetails
